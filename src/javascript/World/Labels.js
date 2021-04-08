9066ba4a91096a2da20969cec41b798fc8686abd
Hsiao Yao <tomtogo1260@gmail.com>
2021-04-09 05:43:21 +0800

    :seedling: label typing feature
    label typing feature

--- src/javascript/World/Labels.js
+++ src/javascript/World/Labels.js
@@ -27,27 +27,36 @@ export default class Labels {
         this.labels.push(this.label1);
         this.labels.push(this.label2);
 
-        this.label1.element = document.createElement('DIV');
-        this.label2.element = document.createElement('DIV');
-        this.label1.element.className = 'point point-1';
-        this.label2.element.className = 'point point-2';
-        this.label1.element.innerText = 'Point 1';
-        this.label2.element.innerText = 'Point 2';
-        this.label1.shift = { x: 10, y: 0 };
-        this.label2.shift = { x: -150, y: 60 };
+        this.label1.container = document.createElement('DIV');
+        this.label2.container = document.createElement('DIV');
+        this.label1.container.className = 'container';
+        this.label2.container.className = 'container';
+
+        this.label1.point = document.createElement('DIV');
+        this.label2.point = document.createElement('DIV');
+        this.label1.point.className = 'point point-1 left';
+        this.label2.point.className = 'point point-2 right';
+        this.label1.point.innerText = '23.5 degree tilt';
+        this.label2.point.innerText = 'only 16g pen weight';
+        // eslint-disable-next-line object-curly-newline
+        this.label1.config = { shiftX: 40, shiftY: 0, k: 0.001, phase: 0.0 * Math.PI, amp: 5 };
+        // eslint-disable-next-line object-curly-newline
+        this.label2.config = { shiftX: -167, shiftY: 35, k: 0.001, phase: 0.5 * Math.PI, amp: 5 };
 
         this.points = document.querySelector('.points');
-        this.points.appendChild(this.label1.element);
-        this.points.appendChild(this.label2.element);
+        this.points.appendChild(this.label1.container);
+        this.points.appendChild(this.label2.container);
+        this.label1.container.appendChild(this.label1.point);
+        this.label2.container.appendChild(this.label2.point);
 
         if (this.debug) {
             this.role.label1.add(new THREE.AxesHelper(0.5));
             this.role.label2.add(new THREE.AxesHelper(0.5));
 
-            this.debugFolder.add(this.label1.shift, 'x').min(-200).max(200).step(1).name('label1 shiftX');
-            this.debugFolder.add(this.label1.shift, 'y').min(-200).max(200).step(1).name('label1 shiftY');
-            this.debugFolder.add(this.label2.shift, 'x').min(-200).max(200).step(1).name('label2 shiftX');
-            this.debugFolder.add(this.label2.shift, 'y').min(-200).max(200).step(1).name('label2 shiftY');
+            this.debugFolder.add(this.label1.config, 'shiftX').min(-300).max(300).step(1).name('label1 shiftX');
+            this.debugFolder.add(this.label1.config, 'shiftY').min(-300).max(300).step(1).name('label1 shiftY');
+            this.debugFolder.add(this.label2.config, 'shiftX').min(-300).max(300).step(1).name('label2 shiftX');
+            this.debugFolder.add(this.label2.config, 'shiftY').min(-300).max(300).step(1).name('label2 shiftY');
         }
     }
 
@@ -58,8 +67,6 @@ export default class Labels {
         this.label2.mesh = this.role.label2;
         this.label1.position = this.label1.mesh.position.clone();
         this.label2.position = this.label2.mesh.position.clone();
-        this.label1.element.classList.add('visible');
-        this.label2.element.classList.add('visible');
 
         this.labels.forEach((value) => {
             const label = value;
@@ -75,10 +82,24 @@ export default class Labels {
                 const label = value;
                 const screenPosition = label.position.clone();
                 screenPosition.project(this.camera.instance);
-                const x = screenPosition.x * this.sizes.width * 0.5 + label.shift.x;
-                const y = -screenPosition.y * this.sizes.height * 0.5 - label.shift.y;
-                label.element.style.transform = `translateX(${x}px) translateY(${y}px)`;
+
+                let x = screenPosition.x * this.sizes.width * 0.5;
+                let y = -screenPosition.y * this.sizes.height * 0.5;
+
+                x += label.config.shiftX;
+                y -= label.config.shiftY;
+
+                const theta = label.config.k * this.time.elapsed + label.config.phase;
+                y -= label.config.amp * Math.sin(theta);
+
+                label.container.style.transform = `translateX(${x}px) translateY(${y}px)`;
             });
         });
+
+        this.label1.point.classList.add('visible');
+        this.label1.point.classList.add('typing');
+        await new Promise((resolve) => setTimeout(resolve, 1500));
+        this.label2.point.classList.add('visible');
+        this.label2.point.classList.add('typing');
     }
 }
