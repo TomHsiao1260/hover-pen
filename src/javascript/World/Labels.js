d693a3ae64cb66dd0a81b846403686cdceb05068
Hsiao Yao <tomtogo1260@gmail.com>
2021-04-13 11:07:22 +0800

    :chart_with_upwards_trend: clearer expression for labels
    clearer expression for labels

--- src/javascript/World/Labels.js
+++ src/javascript/World/Labels.js
@@ -20,6 +20,7 @@ export default class Labels {
     }
 
     async setup() {
+        await this.setData();
         await this.setDOM();
         await this.setPosition();
         await this.setAnimation();
@@ -30,54 +31,61 @@ export default class Labels {
         await this.setHidden();
     }
 
-    // labels DOM
-    setDOM() {
+    setData() {
         this.labels = [];
         this.label1 = {};
         this.label2 = {};
         this.labels.push(this.label1);
         this.labels.push(this.label2);
 
-        this.label1.$container = document.createElement('DIV');
-        this.label2.$container = document.createElement('DIV');
-        this.label1.$container.className = 'container';
-        this.label2.$container.className = 'container';
-
-        this.label1.$point = document.createElement('DIV');
-        this.label2.$point = document.createElement('DIV');
-        this.label1.$point.className = 'point point-1 left';
-        this.label2.$point.className = 'point point-2 right';
-        this.label1.$point.innerText = '23.5 degree tilt';
-        this.label2.$point.innerText = 'only 16g pen weight';
-
-        // labels positioning parameters
-        this.labels.config = { k: 0.0005, amp: 15 };
+        this.label1.name = 'label1';
+        this.label1.className = 'point point-1 left';
+        this.label1.innerText = '23.5 degree tilt';
         this.label1.config = { shiftX: 28, shiftY: 17, phase: 0.0 * Math.PI };
+        this.label1.wait = 1500;
+
+        this.label2.name = 'label2';
+        this.label2.className = 'point point-2 right';
+        this.label2.innerText = 'only 16g pen weight';
         this.label2.config = { shiftX: -200, shiftY: 17, phase: 0.5 * Math.PI };
+        this.label2.wait = 2000;
+
+        this.labels.config = { k: 0.0005, amp: 15 };
+        // labels positioning parameters (on mobile)
         if (this.sizes.width < 768) {
             this.labels.config = { k: 0.0005, amp: 10 };
             this.label1.config = { shiftX: 0, shiftY: -20, phase: 0.0 * Math.PI };
             this.label2.config = { shiftX: -172, shiftY: 8, phase: 0.5 * Math.PI };
         }
+    }
 
+    // labels DOM
+    setDOM() {
         this.$points = document.querySelector('.points');
-        this.$points.appendChild(this.label1.$container);
-        this.$points.appendChild(this.label2.$container);
-        this.label1.$container.appendChild(this.label1.$point);
-        this.label2.$container.appendChild(this.label2.$point);
+
+        this.labels.forEach((value) => {
+            const label = value;
+
+            label.$container = document.createElement('DIV');
+            label.$container.className = 'container';
+            label.$point = document.createElement('DIV');
+            label.$point.className = label.className;
+            label.$point.innerText = label.innerText;
+
+            label.$container.appendChild(label.$point);
+            this.$points.appendChild(label.$container);
+
+            if (this.debug) {
+                this.role[label.name].add(new THREE.AxesHelper(0.5));
+                this.debugFolder.add(label.config, 'shiftX').min(-300).max(300).step(1).name(`${label.name} shiftX`);
+                this.debugFolder.add(label.config, 'shiftY').min(-300).max(300).step(1).name(`${label.name} shiftY`);
+                this.debugFolder.add(label.config, 'phase').min(-4).max(4).step(0.1).name(`${label.name} phase`);
+            }
+        });
 
         if (this.debug) {
-            this.role.label1.add(new THREE.AxesHelper(0.5));
-            this.role.label2.add(new THREE.AxesHelper(0.5));
-
             this.debugFolder.add(this.labels.config, 'k').min(0).max(0.01).step(0.0001).name('labels k');
             this.debugFolder.add(this.labels.config, 'amp').min(0).max(50).step(1).name('labels ampitude');
-            this.debugFolder.add(this.label1.config, 'shiftX').min(-300).max(300).step(1).name('label1 shiftX');
-            this.debugFolder.add(this.label1.config, 'shiftY').min(-300).max(300).step(1).name('label1 shiftY');
-            this.debugFolder.add(this.label1.config, 'phase').min(-4).max(4).step(0.1).name('label1 phase');
-            this.debugFolder.add(this.label2.config, 'shiftX').min(-300).max(300).step(1).name('label2 shiftX');
-            this.debugFolder.add(this.label2.config, 'shiftY').min(-300).max(300).step(1).name('label2 shiftY');
-            this.debugFolder.add(this.label2.config, 'phase').min(-4).max(4).step(0.1).name('label2 phase');
         }
     }
 
@@ -86,14 +94,12 @@ export default class Labels {
         // wait 1 second to make sure all transformation matrices are ready
         await new Promise((resolve) => setTimeout(resolve, 1000));
 
-        this.label1.mesh = this.role.label1;
-        this.label2.mesh = this.role.label2;
-        this.label1.position = this.label1.mesh.position.clone();
-        this.label2.position = this.label2.mesh.position.clone();
-
-        // apply all matrices for each parent by traversing
         this.labels.forEach((value) => {
             const label = value;
+            label.mesh = this.role[label.name];
+            label.position = label.mesh.position.clone();
+
+            // apply all matrices for each parent by traversing
             while (label.mesh.parent !== null) {
                 const matrix = label.mesh.parent.matrix.clone();
                 label.position = label.position.applyMatrix4(matrix);
@@ -127,13 +133,15 @@ export default class Labels {
 
     // display each label with typing effect
     async setDisplay() {
-        this.label1.$point.classList.add('visible');
-        this.label1.$point.classList.add('typing');
-        await new Promise((resolve) => setTimeout(resolve, 1500));
+        await this.labels.reduce(async (promise, value) => {
+            await promise;
 
-        this.label2.$point.classList.add('visible');
-        this.label2.$point.classList.add('typing');
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+            const label = value;
+            label.$point.classList.add('visible');
+            label.$point.classList.add('typing');
+
+            return new Promise((resolve) => setTimeout(resolve, label.wait));
+        }, Promise.resolve());
     }
 
     // hide the label if it's behind the meshes
